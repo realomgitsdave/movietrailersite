6dfa54f912ef97234e146ee54f4ffb719f784cdf
realomgitsdave <dalexander@gmail.com>
2017-07-14 20:25:42 -0400

    Resubmit project based on review feedback

--- media.py
+++ media.py
@@ -1,11 +1,22 @@
 import webbrowser
 
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+
+    def __init__(self, title, storyline, poster_url, trailer_url):
+
+        """ Initialize a movie object
+        Args:
+        title = title of the movie
+        storyline = brief summary of the movie
+        poster_url = url to movie's poster image
+        trailer_url = url to movie's trailer on YouTube
+        """
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_url
+        self.trailer_youtube_url = trailer_url
 
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        """ Open YouTube trailer in web browser """
+        webbrowser.open(self.trailer_url)
